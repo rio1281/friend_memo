9fc63f1d5459b482758b4ff1b74aa06ee413197c
Rio Nishino <n.rio1281@gmail.com>
2013-06-26 14:11:22 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FriendMemo::Application.config.secret_token = 'e0cf25460ae9f37eb72e6a0783642265b89c5bf2f99833c80942b988484ccb7625268ee8d34d1e0b942466d4233096f3d2e80221fe0f05970d18fd1ecf46ca08'
