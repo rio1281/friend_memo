5dfbc5d2bc6411bc64f4c852d6360f31fa0b84c1
Rio Nishino <n.rio1281@gmail.com>
2013-06-27 10:50:40 +0900

    Anable to access graph api by using koala

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  helper_method :current_user
+  helper_method :current_user, :graph
 
   private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def graph
+    # koalaでFacebook Graph APIにアクセス
+    Koala::Facebook::API.new(current_user.token)
   end
 
 end
