0b657376fbc96ba541021a587ffa41a427f59489
Rio Nishino <n.rio1281@gmail.com>
2013-06-29 02:44:55 +0900

    Store name in friend table

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,13 +1,14 @@
 class Friend < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :facebook_id, :memo, :user_id
+  attr_accessible :facebook_id, :memo, :user_id, :name
 
   def self.create_new_by_facebook_api(token)
     user = User.find_by_token(token)
     graph = Koala::Facebook::API.new(token)
     friends = graph.get_connections("me", "friends")
     friends.each do |friend|
-      Friend.find_or_create_by_user_id_and_facebook_id(user_id: user.id, facebook_id: friend["id"])
+      Friend.find_or_create_by_user_id_and_facebook_id(user_id: user.id, facebook_id: friend["id"], name: friend["name"])
     end
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130628105430) do
+ActiveRecord::Schema.define(:version => 20130628172611) do
 
   create_table "friends", :force => true do |t|
     t.string   "facebook_id"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130628105430) do
     t.integer  "user_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "name"
   end
 
   add_index "friends", ["user_id"], :name => "index_friends_on_user_id"
