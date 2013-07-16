f64fd5471891e2a5ece958340d9bce5a190f9313
Rio Nishino <n.rio1281@gmail.com>
2013-07-17 01:05:53 +0900

    Implement feature which displays memo on facebook page

--- myscript.js
+++ myscript.js
@@ -1,8 +1,15 @@
-$("#pageLogo a").text("RIORIO");
+//FacebookからfacebookIDを含む文字列を取得
+var a = $("div#pagelet_timeline_main_column").attr("data-gt");
 
+//Friendテーブルに保存されたfacebook_idが、上記で取得した文字列に含まれる場合、そのfacebook_idに対応するメモを表示
 $.getJSON("http://0.0.0.0:3000/friends.json", function(json){
-  for (var i = 0; i < json.length; ++i) {
-    console.log(json[i]);
+  for (var i = 0; i < json.length;) {
+    if (a.indexOf(json[i].facebook_id) == 18) {
+      $("a._8_2").append("<br>");
+      $("a._8_2").append(json[i].memo);
+      break;
+    } else {
+      i = i + 1;
+    }
   }
-  console.log($("a.passiveName").attr("data-hovercard"));
 });
